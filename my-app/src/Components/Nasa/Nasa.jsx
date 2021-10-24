3810ca611a98f63b2c35c2f6d941efa5a8fa95c4
Ivan <Ivan_567.92@mail.ru>
2021-10-24 11:34:23 +0300

    realise design

--- my-app/src/Components/Nasa/Nasa.jsx
+++ my-app/src/Components/Nasa/Nasa.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react'
 import { useSelector } from 'react-redux'
 import { useDispatch } from 'react-redux'
+import { useRef } from 'react'
 import style from './Nasa.module.css'
 import { getNasaData, nasaFilterLike } from '../../redux/actions/nasaAC'
 import NasaItem from '../NasaItem/NasaItem'
@@ -8,28 +9,36 @@ import NasaItem from '../NasaItem/NasaItem'
 function Nasa() {
     const dispatch = useDispatch()
     const nasaData = useSelector((state) => state.nasaData.nasaData)
-    console.log('redux state >>', nasaData)
     useEffect(() => {
         dispatch(getNasaData())
     }, [])
-
     const [flagFilter, setflagFilter] = useState(false);
+    const [text, setText] = useState('нет лайков');
+    const listPosts = useRef(null);
 
-    const toggleFunck = (flagFilter) => {
+    const likePosts = (flagFilter) => {
+        setflagFilter(!flagFilter)
+        dispatch(nasaFilterLike())
+    }
+
+    const allPosts = (flagFilter) => {
         setflagFilter(!flagFilter)
         dispatch(nasaFilterLike())
     }
 
     return (
         <>
-            {nasaData.length > 0 ?
-                <button className={style.buttonShowPosts} onClick={() => toggleFunck(flagFilter)}>
+            {nasaData.length > 0 && flagFilter === false ?
+                <button className={style.buttonShowPosts} onMouseDown={() => likePosts(flagFilter)}>
                     {flagFilter === true ? 'Показать все посты'
                         : 'Показать посты с лайками'}
                 </button>
-                : ''
+                : <button className={style.buttonShowPosts} onMouseUp={() => allPosts(flagFilter)}>
+                    {flagFilter === true ? 'Показать все посты'
+                        : 'Показать посты с лайками'}
+                </button>
             }
-            <ul className={style.nasaList}>
+            <ul ref={listPosts} className={style.nasaList}>
                 {
                     nasaData.length ?
                         (flagFilter === true ? nasaData.filter(el => el.like === true) : nasaData).map((item) =>
